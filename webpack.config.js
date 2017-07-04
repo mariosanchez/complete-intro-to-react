02030436b63717eb255fb7d44725cfae9efe7f81
Mario Sánchez <mr.mario.sm@gmail.com>
2017-07-04 15:36:25 +0200

    Tools

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path'); // module that resolves relative paths for us
+
+module.exports = {
+    context: __dirname, // it says we are runing webpack in the root directory always
+    entry: './js/ClientApp.jsx', // frontdoor to our project
+    devtool: "cheap-eval-source-map", // it says to inline all my sourcemaps in dev version
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    stats: { // report format and info
+        colors: true,
+        reasons: true,
+        chunks: true
+    },
+    module: {
+        rules: [ // list of loaders; tools that webpack is using on out code
+            {
+                test: /\.jsx?$/, // function or regex
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
