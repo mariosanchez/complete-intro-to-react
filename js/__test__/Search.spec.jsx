670a887512a0a68361b1031542406b2443e0603e
Mario Sánchez <mr.mario.sm@gmail.com>
2017-09-12 23:04:53 +0200

    Testing React

---
+++ js/__test__/Search.spec.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { shallow } from "enzyme";
+import preload from "../../data.json";
+import Search from "../Search";
+import ShowCard from "../ShowCard";
+
+test("Search renders correctly", () => {
+  const component = shallow(<Search />);
+  expect(component).toMatchSnapshot();
+});
+
+test("Should render a correct amount of shows", () => {
+  const component = shallow(<Search />);
+  expect(component.find(ShowCard).length).toEqual(preload.shows.length); // We can use React components in find()!!
+});
+
+test("Should render a correct amount of shows based on term", () => {
+  const searchWord = "black";
+  const component = shallow(<Search />);
+  component
+    .find("input") // We can also use any 'CSS' selectors
+    .simulate("change", { target: { value: searchWord } }); // this last object simulates the event object
+
+  // TODO Separate this filter action in a module an use it in Search.jsx as well, this is a workarround for learn tests
+  const showCount = preload.shows.filter(
+    show =>
+      `${show.title} ${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+
+  expect(component.find(ShowCard).length).toEqual(showCount);
+});
