36169e7eb8eef99e5a4b9ff360377e916b6e2365
Mario Sánchez <mr.mario.sm@gmail.com>
2017-10-04 21:59:52 +0200

    Add Flow types to Redux code

--- js/reducers.js
+++ js/reducers.js
@@ -1,8 +1,7 @@
-import { SET_SEARCH_TERM } from './actions';
+// @flow
 
-const DEFAULT_STATE = {
-  searchTerm: '',
-};
+import { combineReducers } from 'redux';
+import { SET_SEARCH_TERM } from './actions';
 
 // Flow Standard Actions
 /*
@@ -14,17 +13,14 @@ const DEFAULT_STATE = {
 }
 */
 
-const setsSearchTerm = (state, action) =>
-  // It returns a new object copying the properties
-  Object.assign({}, state, { searchTerm: action.payload });
-
-const rootReducer = (state = DEFAULT_STATE, action) => {
-  switch (action.type) {
-    case SET_SEARCH_TERM:
-      return setsSearchTerm(state, action);
-    default:
-      return state;
+const searchTerm = (state = '', action: Action) => {
+  if (action.type === SET_SEARCH_TERM) {
+    return action.payload;
   }
-};
+
+  return state;
+}
+
+const rootReducer = combineReducers({ searchTerm });
 
 export default rootReducer;
